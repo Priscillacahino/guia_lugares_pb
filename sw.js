6d9ff1f6a1faec74347fef7d1cd75fbda3367640
Priscilla Cahino <priscillacahinoo@gmail.com>
2026-09-18 12:54:33 -0300

    Atualiza icone do app e mantem correcoes finais

--- sw.js
+++ sw.js
@@ -1,5 +1,5 @@
-const CACHE="guia-venus-v4";
-const ASSETS=["./","index.html","style.css","dados.js","app.js","manifest.webmanifest","icon-192.png","icon-512.png","venus-gata.jpeg"];
+const CACHE="guia-venus-v5";
+const ASSETS=["./","index.html","style.css","dados.js","app.js","manifest.webmanifest","icon-localizacao-192.png","icon-localizacao-512.png","icon-192.png","icon-512.png","venus-gata.jpeg"];
 self.addEventListener("install",e=>e.waitUntil(caches.open(CACHE).then(c=>c.addAll(ASSETS)).then(()=>self.skipWaiting())));
 self.addEventListener("activate",e=>e.waitUntil(caches.keys().then(keys=>Promise.all(keys.filter(k=>k!==CACHE).map(k=>caches.delete(k)))).then(()=>self.clients.claim())));
 self.addEventListener("fetch",e=>{
